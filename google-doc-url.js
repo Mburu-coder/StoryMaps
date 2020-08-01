534462c0a8a012920703e63254f3aa5621b2d382
Mburu-coder <60397100+Mburu-coder@users.noreply.github.com>
2020-08-01 19:56:20 +0300

    Update

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1M6q72plc71J8RgL1MSGKgghvOOsmwuWZtqfUCLOkwek/edit#gid=0';
